db2bf88f25d2a03640155ae0028b2a7784dd36e0
Marton Illes <marton.illes@gmail.com>
2022-08-31 15:39:53 +0200

    Changed python-lzo dependency
    We do not want it to depend on a specific version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(name='vmlinux-to-elf',
       author_email='',
       url='https://github.com/marin-m/vmlinux-to-elf',
       install_requires=['lz4', 'zstandard',
-        'python-lzo @ git+https://github.com/clubby789/python-lzo@b4e39df'],
+        'python-lzo'],
       packages=['vmlinux_to_elf', 'vmlinux_to_elf.utils'],
       scripts=['vmlinux-to-elf', 'kallsyms-finder']
      )
